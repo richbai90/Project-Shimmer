3dcbf83a6ec5920039a6bacf8abcceda58cde633
Rich <rgordon@bittercreektech.com>
2018-08-03 12:22:56 -0600

    use default preset

--- src/getPageContext.js
+++ src/getPageContext.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 
 import { SheetsRegistry, create } from 'jss';
-import { createMuiTheme, createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+import { createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
 import expand from 'jss-expand';
 import extend from 'jss-extend';
+import jssPreset from 'jss-preset-default';
 
 // Configure JSS
-const jss = create({ plugins: [extend(), ...jssPreset().plugins, expand()] });
+const jss = create(jssPreset());
 
 // A theme with custom primary and secondary color.
 // It's optional.
